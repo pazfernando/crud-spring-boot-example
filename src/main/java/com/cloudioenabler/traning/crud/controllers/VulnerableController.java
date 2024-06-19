ee042c35785c0c54828846d158a485881784acff
Fernando Paz <paz.fernando@gmail.com>
2024-06-19 17:43:59 -0500

    include an intentional vulnerability

--- src/main/java/com/cloudioenabler/traning/crud/controllers/VulnerableController.java
+++ src/main/java/com/cloudioenabler/traning/crud/controllers/VulnerableController.java
@@ -1,10 +1,12 @@
     package com.cloudioenabler.traning.crud.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class VulnerableController {
     @GetMapping("/vulnerable")
     public String vulnerable(@RequestParam("user") String user) {
@@ -13,5 +15,19 @@ public class VulnerableController {
         // Ejecutar consulta en la base de datos...
         
         return "result";
+    }
+
+    @PostMapping("/deserialize")
+    public void deserializeObject(@RequestBody Object obj) {
+        // Código vulnerable a la deserialización insegura
+        // ...
+ 
+ 
+    }
+
+    @PostMapping("/vulnerable")
+    public String vulnerable2(@RequestBody String input) {
+        // Vulnerable a XSS (Cross-Site Scripting)
+        return "Hello, " + input + "!";
     }
 }
