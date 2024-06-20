d1f67de78251ad80bea5dc10b6b7c49e37878e4c
Fernando Paz <paz.fernando@gmail.com>
2024-06-20 14:09:42 -0500

    add Terraform file for QA deployment

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "null_resource" "deploy_docker" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker login -u ${var.docker_user} -p ${var.docker_password} ${var.docker_registry}",
+      "sudo docker pull ${var.docker_registry}/${var.docker_image}:latest",
+      "random_port=$(shuf -i 10000-65535 -n 1)",
+      "sudo docker run -d -p $random_port:8080 ${var.docker_registry}/${var.docker_image}:latest",
+      "echo $random_port > /tmp/deployed_port"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.private_key_path)
+      host        = var.instance_ip
+    }
+  }
+
+  depends_on = [aws_instance.app_instance]
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "access_key" {
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key for SSH"
+}
+
+variable "docker_user" {
+  description = "Docker registry user"
+}
+
+variable "docker_password" {
+  description = "Docker registry password"
+}
+
+variable "docker_registry" {
+  description = "Docker registry URL"
+  default     = "registry.gitlab.com"
+}
+
+variable "docker_image" {
+  description = "Docker image name"
+}
+
+variable "instance_ip" {
+  description = "IP address of the existing EC2 instance"
+}
+
+output "deployed_port" {
+  value = file("/tmp/deployed_port")
+}
